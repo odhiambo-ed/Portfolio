000f40c502897a4d1d7b9566c7d2be388bd720ab
sangwa7 <didiersangwa@gmail.com>
2022-03-01 10:28:57 +0200

    Second Linters update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const mobileBtn = document.getElementById('hamburger')
-              nav = document.querySelector('nav')
-              mobileBtnExit = document.getElementById('mobile-exit');
+nav = document.querySelector('nav')
+mobileBtnExit = document.getElementById('mobile-exit');
 
-        mobileBtn.addEventListener('click', () => {
-            nav.classList.add('menu-btn');
-        })
+mobileBtn.addEventListener('click', () => {
+    nav.classList.add('menu-btn');
+})
 
-        mobileBtnExit.addEventListener('click', () => {
-            nav.classList.remove('menu-btn');
-        })
+mobileBtnExit.addEventListener('click', () => {
+    nav.classList.remove('menu-btn');
+})

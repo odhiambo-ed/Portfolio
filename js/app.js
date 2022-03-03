cf2fd18be99efa1c5699069f483c7c9c0620c691
sangwa7 <didiersangwa@gmail.com>
2022-03-03 12:05:14 +0200

    Add JS form validation

--- js/app.js
+++ js/app.js
@@ -296,3 +296,26 @@ document.addEventListener('click', (e) => {
     document.querySelector('body').style.overflowY = 'hidden';
   }
 });
+
+/*
+ Email Validation
+ */
+
+ document.querySelector("#contact-form").addEventListener("submit", e =>{
+  let valid = true;
+
+  let responseContainer = document.querySelector("#response-text");
+
+  let emailArray = document.querySelector("#email").value.split('');
+
+  for(let i = 0; i < emailArray.length; i++){
+    if(emailArray[i] != emailArray[i].toLowerCase() && emailArray[i] != "@" && emailArray[i] != ".") {
+      valid = false;
+    }
+  }
+
+  if(!valid){
+    e.preventDefault();
+    responseContainer.innerHTML = "Invalid Email";
+  }
+});

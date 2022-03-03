d76308af3405b4526021e8fd6f712e75e4de27d0
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-03 22:54:46 +0300

    Update Local Storage Function

--- js/app.js
+++ js/app.js
@@ -306,8 +306,6 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
   const responseContainer = document.querySelector('#response-text');
 
   const email = document.querySelector('#email').value;
-  const name = document.querySelector('#contact-name').value;
-  const message = document.querySelector('#contact-message').value;
 
   const emailArray = email.split('');
 
@@ -320,23 +318,10 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     }
   }
 
-  // if the email is valid, save user data in object
+  // if the email is valid, proceed with form submission
   if (valid) {
-    // prevent the form fro automatically reloading the page
-    e.preventDefault();
-
     // empty the response test in contact form
     responseContainer.innerHTML = '';
-    // save the user info to object
-    const details = {
-      name,
-      email,
-      message,
-    };
-
-    // save the user object to localStorage
-    // (here we use JSON.stringify() to convert the user data to string for safe storage)
-    localStorage.setItem('contact-details', JSON.stringify(details));
   } else {
     e.preventDefault();
     responseContainer.innerHTML = 'Invalid Email';
@@ -345,13 +330,44 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
 
 // function to populate the contact form from localStorage
 function loadFormData() {
-  if (localStorage.getItem('contact-details') != null) {
-    const userData = JSON.parse(localStorage.getItem('contact-details'));
+  const userData = JSON.parse(localStorage.getItem('contact-details'));
+  document.querySelector('#email').value = userData.email;
+  document.querySelector('#contact-name').value = userData.name;
+  document.querySelector('#contact-message').value = userData.message;
+}
 
-    document.querySelector('#email').value = userData.email;
-    document.querySelector('#contact-name').value = userData.name;
-    document.querySelector('#contact-message').value = userData.message;
+function initializeStorage() {
+  if (localStorage.getItem('contact-details') == null) {
+    const details = {
+      name: '',
+      email: '',
+      message: '',
+    };
+
+    localStorage.setItem('contact-details', JSON.stringify(details));
   }
 }
 
+function updateStorage() {
+  const details = JSON.parse(localStorage.getItem('contact-details'));
+  details.email = document.querySelector('#email').value;
+  details.name = document.querySelector('#contact-name').value;
+  details.message = document.querySelector('#contact-message').value;
+  localStorage.setItem('contact-details', JSON.stringify(details));
+}
+
+initializeStorage();
 loadFormData();
+
+// Event listeners to save data when ser populates contact form
+document.querySelector('#email').addEventListener('keyup', () => {
+  updateStorage();
+});
+
+document.querySelector('#contact-name').addEventListener('keyup', () => {
+  updateStorage();
+});
+
+document.querySelector('#contact-message').addEventListener('keyup', () => {
+  updateStorage();
+});

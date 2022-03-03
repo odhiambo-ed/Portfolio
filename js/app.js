79b59ce8cceef074f57b0be3acac0e950bad7eed
sangwa7 <didiersangwa@gmail.com>
2022-03-03 14:42:34 +0200

    Add Local storage function

--- js/app.js
+++ js/app.js
@@ -301,12 +301,16 @@ document.addEventListener('click', (e) => {
  Email Validation
  */
 
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
+ document.querySelector('#contact-form').addEventListener('submit', (e) => {
   let valid = true;
 
   const responseContainer = document.querySelector('#response-text');
 
-  const emailArray = document.querySelector('#email').value.split('');
+  const email = document.querySelector('#email').value;
+  const name = document.querySelector('#contact-name').value;
+  const message = document.querySelector('#contact-message').value;
+
+  const emailArray = email.split('');
 
   for (let i = 0; i < emailArray.length; i += 1) {
     if (
@@ -317,8 +321,38 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     }
   }
 
-  if (!valid) {
+  // if the email is valid, save user data in object
+  if (valid) {
+    // prevent the form fro automatically reloading the page
+    e.preventDefault();
+
+    // empty the response test in contact form
+    responseContainer.innerHTML = '';
+    // save the user info to object
+    const details = {
+      name,
+      email,
+      message,
+    };
+
+    // save the user object to localStorage
+    // (here we use JSON.stringify() to convert the user data to string for safe storage)
+    localStorage.setItem('contact-details', JSON.stringify(details));
+  } else {
     e.preventDefault();
     responseContainer.innerHTML = 'Invalid Email';
   }
 });
+
+// function to populate the contact form from localStorage
+function loadFormData() {
+  if (localStorage.getItem('contact-details') != null) {
+    const userData = JSON.parse(localStorage.getItem('contact-details'));
+
+    document.querySelector('#email').value = userData.email;
+    document.querySelector('#contact-name').value = userData.name;
+    document.querySelector('#contact-message').value = userData.message;
+  }
+}
+
+loadFormData();
